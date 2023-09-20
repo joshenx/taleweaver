07bca03e98622ada96a65bcc31b042ce18a93c4f
Neo Wei Qing <ztolenstar@hotmail.com>
2023-09-20 21:38:33 +0800

    remove prev

--- src/App/WhiteLogo.tsx
+++
@@ -1,8 +0,0 @@
-import { chakra, forwardRef, ImageProps } from '@chakra-ui/react';
-import * as React from 'react';
-
-import logo from '/src/images/taleweaver_logo_light_svg.svg';
-
-export const WhiteLogo = forwardRef<ImageProps, 'img'>((props, ref) => {
-  return <chakra.img src={logo} ref={ref} width={'120px'} {...props} />;
-});
