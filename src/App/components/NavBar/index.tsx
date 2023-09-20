69660b7599b33fadacc8fe4fd3d0b6a84d0fc54a
joshenx <e0525998@u.nus.edu>
2023-09-20 19:01:06 +0800

    Add better navbar

--- src/App/components/NavBar/index.tsx
+++ src/App/components/NavBar/index.tsx
@@ -1,38 +1,114 @@
-import './style.css';
+'use client';
 
-import { CloseIcon, HamburgerIcon, MoonIcon, SunIcon } from '@chakra-ui/icons';
 import {
-  Avatar,
   Box,
-  Button,
-  Center,
   Flex,
-  HStack,
+  Text,
   IconButton,
-  Link,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuList,
+  Button,
   Stack,
-  useColorMode,
-  useColorModeValue,
-  useDisclosure,
   Collapse,
-  Hide,
-  Divider,
+  Icon,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
 } from '@chakra-ui/react';
-import { ReactNode } from 'react';
 import { Link as ReachLink } from 'react-router-dom';
-
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from '@chakra-ui/icons';
 import pagesData from '../../../pages/pagesData';
 import { routerType } from '../../../types/router.types';
 import { DefaultLogo } from '../../DefautLogo';
 
-const NavBar = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const { isOpen, onOpen, onClose } = useDisclosure();
+export default function NavBar() {
+  const { isOpen, onToggle } = useDisclosure();
 
+  return (
+    <Box>
+      <Flex
+        bg={useColorModeValue('white', 'gray.800')}
+        color={useColorModeValue('gray.600', 'white')}
+        minH={'60px'}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+        align={'center'}
+      >
+        <Flex
+          flex={{ base: 1, md: 'auto' }}
+          ml={{ base: -2 }}
+          display={{ base: 'flex', md: 'none' }}
+        >
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant={'ghost'}
+            aria-label={'Toggle Navigation'}
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          <DefaultLogo />
+
+          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={'flex-end'}
+          direction={'row'}
+          spacing={6}
+        >
+          <Button
+            as={'a'}
+            fontSize={'sm'}
+            fontWeight={400}
+            variant={'link'}
+            href={'#'}
+          >
+            Sign In
+          </Button>
+          <Button
+            as={'a'}
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'red.400'}
+            href={'#'}
+            _hover={{
+              bg: 'red.600',
+            }}
+          >
+            Sign Up
+          </Button>
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  );
+}
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue('gray.600', 'gray.200');
+  const linkHoverColor = useColorModeValue('gray.800', 'white');
+  const popoverContentBgColor = useColorModeValue('white', 'gray.800');
   const NavLink = ({
     children,
     link,
@@ -57,117 +133,205 @@ const NavBar = () => {
   );
 
   return (
-    <>
-      <Flex
-        as="header"
-        position="fixed"
-        top="0"
-        className="gradient-header"
-        backdropFilter="contrast(98%) blur(5px)"
-        boxShadow="0px 0px 30px 0px rgba(0, 0, 0, 0.15)"
-        p="0.5rem 1rem"
-        w="100%"
-        alignItems={'center'}
-        justifyContent={'space-between'}
-        zIndex="overlay"
-      >
-        <IconButton
-          size={'md'}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={'Open Menu'}
-          display={{ md: 'none' }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack spacing={8} alignItems={'center'}>
-          <DefaultLogo pl="1rem" my="0.5rem" />
-          {/* <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-            {pagesData
-              .filter(({ path }: routerType) => path !== '*')
-              .map(({ path, title }: routerType, index) => (
-                <NavLink key={index} link={path}>
-                  {title}
-                </NavLink>
-              ))}
-          </HStack> */}
-        </HStack>
-
-        <Flex alignItems={'center'}>
-          <Stack direction={'row'} spacing={7}>
-            {/* <Button onClick={toggleColorMode}>
-              {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-            </Button> */}
-
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}
+    <Stack direction={'row'} spacing={4}>
+      {/* {pagesData
+        .filter(({ path }: routerType) => path !== '*')
+        .map(({ path, title }: routerType, index) => (
+          <NavLink key={index} link={path}>
+            {title}
+          </NavLink>
+        ))} */}
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={'hover'} placement={'bottom-start'}>
+            <PopoverTrigger>
+              <Box
+                as="a"
+                p={2}
+                href={navItem.href ?? '#'}
+                fontSize={'sm'}
+                fontWeight={500}
+                color={linkColor}
+                _hover={{
+                  textDecoration: 'none',
+                  color: linkHoverColor,
+                }}
               >
-                <Avatar
-                  size={'sm'}
-                  src={'https://avatars.dicebear.com/api/male/username.svg'}
-                />
-              </MenuButton>
-              <MenuList alignItems={'center'}>
-                <br />
-                <Center>
-                  <Avatar
-                    size={'2xl'}
-                    src={'https://avatars.dicebear.com/api/male/username.svg'}
-                  />
-                </Center>
-                <br />
-                <Center>
-                  <p>Average TaleWeaver Enjoyer</p>
-                </Center>
-                <br />
-                <MenuDivider />
-                {/* <MenuItem>Your Servers</MenuItem>
-                <MenuItem>Account Settings</MenuItem>
-                <MenuItem>Logout</MenuItem> */}
-              </MenuList>
-            </Menu>
-          </Stack>
-        </Flex>
-      </Flex>
+                {navItem.label}
+              </Box>
+            </PopoverTrigger>
 
-      <Hide above="md">
-        <Collapse
-          in={isOpen}
-          animateOpacity
-          style={{ marginTop: '0!important' }}
-        >
-          <Stack
-            my={3}
-            pl={4}
-            backdropFilter="contrast(98%) blur(5px)"
-            boxShadow="0px 0px 30px 0px rgba(0, 0, 0, 0.15)"
-            borderRadius="5px"
-            mt="20"
-            position="fixed"
-            borderLeft={1}
-            zIndex="overlay"
-            borderStyle={'solid'}
-            fontSize="xl"
-            textTransform="uppercase"
-            borderColor={useColorModeValue('gray.200', 'gray.700')}
-            align={'start'}
-          >
-            {pagesData
-              .filter(({ path }: routerType) => path !== '*')
-              .map(({ path, title }: routerType, index) => (
-                <NavLink key={index} link={path}>
-                  {title}
-                </NavLink>
-              ))}
-          </Stack>
-          <Divider />
-        </Collapse>
-      </Hide>
-    </>
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={'xl'}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={'xl'}
+                minW={'sm'}
+              >
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
   );
 };
 
-export default NavBar;
+const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
+  return (
+    <Box
+      as="a"
+      href={href}
+      role={'group'}
+      display={'block'}
+      p={2}
+      rounded={'md'}
+      _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}
+    >
+      <Stack direction={'row'} align={'center'}>
+        <Box>
+          <Text
+            transition={'all .3s ease'}
+            _groupHover={{ color: 'pink.400' }}
+            fontWeight={500}
+          >
+            {label}
+          </Text>
+          <Text fontSize={'sm'}>{subLabel}</Text>
+        </Box>
+        <Flex
+          transition={'all .3s ease'}
+          transform={'translateX(-10px)'}
+          opacity={0}
+          _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+          justify={'flex-end'}
+          align={'center'}
+          flex={1}
+        >
+          <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+        </Flex>
+      </Stack>
+    </Box>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Stack
+      bg={useColorModeValue('white', 'gray.800')}
+      p={4}
+      display={{ md: 'none' }}
+    >
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const MobileNavItem = ({ label, children, href }: NavItem) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <Box
+        py={2}
+        as="a"
+        href={href ?? '#'}
+        justifyContent="space-between"
+        alignItems="center"
+        _hover={{
+          textDecoration: 'none',
+        }}
+      >
+        <Text
+          fontWeight={600}
+          color={useColorModeValue('gray.600', 'gray.200')}
+        >
+          {label}
+        </Text>
+        {children && (
+          <Icon
+            as={ChevronDownIcon}
+            transition={'all .25s ease-in-out'}
+            transform={isOpen ? 'rotate(180deg)' : ''}
+            w={6}
+            h={6}
+          />
+        )}
+      </Box>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={'solid'}
+          borderColor={useColorModeValue('gray.200', 'gray.700')}
+          align={'start'}
+        >
+          {children &&
+            children.map((child) => (
+              <Box as="a" key={child.label} py={2} href={child.href}>
+                {child.label}
+              </Box>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+};
+interface NavItem {
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: 'Inspiration',
+    children: [
+      {
+        label: 'Explore Design Work',
+        subLabel: 'Trending Design to inspire you',
+        href: '#',
+      },
+      {
+        label: 'New & Noteworthy',
+        subLabel: 'Up-and-coming Designers',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Find Work',
+    children: [
+      {
+        label: 'Job Board',
+        subLabel: 'Find your dream design job',
+        href: '#',
+      },
+      {
+        label: 'Freelance Projects',
+        subLabel: 'An exclusive list for contract work',
+        href: '#',
+      },
+    ],
+  },
+  {
+    label: 'Learn Design',
+    href: '#',
+  },
+  {
+    label: 'Hire Designers',
+    href: '#',
+  },
+];
