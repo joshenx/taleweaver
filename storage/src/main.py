35d90a8ab4e916fcafbe333803f11e3c8d689066
Neo Wei Qing <ztolenstar@hotmail.com>
2023-09-24 02:51:14 +0800

    data persistence

--- storage/src/main.py
+++ storage/src/main.py
@@ -12,8 +12,7 @@ sys.path.append(str(Path(__file__).parent.parent.parent))
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException
-from common.database import SessionLocal
-from src.supabase_api import get_users
+from src.supabase_api import get_users, get_stories_by_user, get_story_by_id, save_users_story
 
 app = FastAPI()
 
@@ -40,7 +39,42 @@ key = os.environ.get("VITE_SUPABASE_ANON_KEY")
 supabase: Client = create_client(url, key)
 
 @app.get("/get-all-users")
-async def root():
+async def get_all_users():
+    # no particular return format settled
     return get_users(supabase)
 
+@app.get("/{user_id}/get-all-stories")
+async def get_users_stories(user_id: int):
+    # no particular return format settled
+    return get_stories_by_user(supabase, user_id)
+
+@app.get("/{story_id}/get-story")
+async def get_story(story_id: int):
+    # same return format as with the genapi
+    return get_story_by_id(supabase, story_id)
+
+@app.post("/save-story") # TODO: check if story data type needs to be converted
+async def save_story(user_id: int, story: dict):
+    # user_id = 1
+    # story = {
+    #     "title": "The Adventures of Teddy",
+    #     "focus": "vocabulary",
+    #     "vocabulary_age": "3",
+    #     "total_pages": "1",
+    #     "story": [
+    #         {"page": 1,
+    #          "text": "Once upon a time, there was a teddy bear named Teddy. Teddy loved to go on adventures and explore the world around him.",
+    #          "image_prompt": "Teddy bear playing in a colorful garden",
+    #          "subject_description": "A fluffy brown teddy bear with a big smile on his face",
+    #          "image_url": "https://oaidalleapiprodscus.blob.core.windows.net/private/org-PuMGCWJ1M3tJ6ExWwgZAlVT4/user-TETd6CWnI82tNSrj9rzXTm2Z/img-bAHOYMVZYaHoxhki7ijZ3wIC.png?st=2023-09-21T16%3A02%3A42Z&se=2023-09-21T18%3A02%3A42Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-09-20T21%3A55%3A13Z&ske=2023-09-21T21%3A55%3A13Z&sks=b&skv=2021-08-06&sig=1636ZkX6Wk18Neqpt%2BkucftDkjSc9RESrdE19MaZcvE%3D"},
+    #         {"page": 2,
+    #          "text": "Then, there was a teddy bear named Teddy. Teddy loved to go on adventures and explore the world around him.",
+    #          "image_prompt": "Teddy bear playing in a colorful garden",
+    #          "subject_description": "A fluffy brown teddy bear with a big smile on his face",
+    #          "image_url": "https://placekitten.com/200/300"}
+    #     ]
+    # }
+    
+    # returns an int, which is the story id
+    return save_users_story(supabase, user_id, story)
 
--- storage/src/supabase_api.py
+++ storage/src/supabase_api.py
@@ -1,5 +1,84 @@
 from supabase import Client
+import json
+import requests
 
 def get_users(db: Client):
-    users = db.table("users").select("*").execute()
+    users = db.table('users').select('userid').execute().model_dump_json()
+    users = json.loads(users)
     return users['data']
+
+def get_stories_by_user(db: Client, user_id: int):
+    stories = db.table('stories').select('*').eq('userid', user_id).execute().model_dump_json()
+    stories = json.loads(stories)
+    return stories['data']
+
+def get_story_by_id(db: Client, story_id: int):
+    story_info = db.table('stories').select('age', 'focus', 'title').eq('storyid', story_id).execute().model_dump_json()
+    story_info = json.loads(story_info)['data'][0]
+
+    pages = db.table('pages').select('*').eq('storyid', story_id).execute().model_dump_json()
+    pages = json.loads(pages)['data']
+    story = {
+        "title": story_info['title'],
+        "focus": story_info['focus'],
+        "vocabulary_age": story_info['age'],
+        "total_pages": len(pages),
+        "story": []
+    }
+
+    pages.sort(key=lambda x: x['pagenumber'])
+    for page in pages:
+        contents = db.table('contents').select('pagetext', 'imageprompt', 'subjdesc', 'imageurl').eq('pageid', page['pageid']).execute().model_dump_json()
+        contents = json.loads(contents)['data'][0]
+        story['story'].append({
+            "page": page['pagenumber'],
+            "text": contents['pagetext'],
+            "image_prompt": contents['imageprompt'],
+            "subject_description": contents['subjdesc'],
+            "image_url": contents['imageurl']
+        })
+    return json.dumps(story)
+
+def save_users_story(db: Client, user_id: int, story: dict):
+    story_id = save_story_info(db, user_id, story['title'])
+    save_story_pages(db, story_id, story['story'])
+    return story_id
+
+def save_story_info(db: Client, user_id: int, title: str):
+    story_info = {
+        "userid": user_id,
+        "title": title,
+    }
+    response = db.table('stories').insert(story_info).execute().model_dump_json()
+    story_id = json.loads(response)['data'][0]['storyid']
+    return story_id
+
+def save_story_pages(db: Client, story_id: int, pages: list):
+    for page in pages:
+        page_info = {
+            "storyid": story_id,
+            "pagenumber": page['page']
+        }
+        response = db.table('pages').insert(page_info).execute().model_dump_json()
+        page_id = json.loads(response)['data'][0]['pageid']
+
+        image_url = save_image(db, story_id, page['page'], page['image_url'])
+
+        save_page_contents(db, page_id, page['text'], page['image_prompt'], page['subject_description'], image_url)
+
+def save_image(db: Client, story_id: int, page_number: int, image_url: str):
+    data = requests.get(image_url)
+    image = data.content
+    path = f'{story_id}/{page_number}.png'
+    db.storage.get_bucket('images').upload(path, image)
+    return db.storage.get_bucket('images').get_public_url(path)
+
+def save_page_contents(db: Client, page_id: int, text: str, image_prompt: str, subject_description: str, image_url: str):
+    content_info = {
+        "pageid": page_id,
+        "pagetext": text,
+        "imageprompt": image_prompt,
+        "subjdesc": subject_description,
+        "imageurl": image_url
+    }
+    db.table('contents').insert(content_info).execute()
