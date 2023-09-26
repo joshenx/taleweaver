084938244d38376e68bb76cda517cdfcaf157911
jivesh <jiveshm2000@gmail.com>
2023-09-27 00:32:30 +0800

    Initialize tracking and track pageviews

--- frontend/src/pages/router.tsx
+++ frontend/src/pages/router.tsx
@@ -2,8 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import { routerType } from '../types/router.types';
 import pagesData from './pagesData';
 import AuthRoute from '../App/components/AuthRoute';
+import { useEffect } from 'react';
+import ReactGA4 from 'react-ga4';
 
 const Router = () => {
+  useEffect(() =>
+    ReactGA4.send({ 
+      hitType: 'pageview',
+      page: window.location.pathname + window.location.search
+    }), []);
+
   const pageRoutes = pagesData.map(
     ({ path, title, element, authRequired }: routerType) => {
       return !authRequired ? (
